553e560306255b5eb404fd932bd4538324976522
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-03-23 09:33:16 -0400

    show broken and working version side-by-side

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import solidSvg from "vite-plugin-solid-svg";
-// import devtools from 'solid-devtools/vite';
+// switch commented import to see error
+// import solidSvg from "vite-plugin-solid-svg";
+import solidSvg from "vite-plugin-solid-svg-updated";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-    solidSvg(),
-  ],
+  plugins: [solidPlugin(), solidSvg()],
   server: {
     port: 3000,
   },
